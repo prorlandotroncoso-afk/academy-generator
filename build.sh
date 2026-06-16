d5f1813e76b73ebe7b357b8d71c4d84349361037
Orlando <prorlandotroncoso@gmail.com>
2026-06-16 00:00:03 -0300

    fix render OCR install

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y tesseract-ocr poppler-utils
+apt-get update || true
+apt-get install -y tesseract-ocr poppler-utils || true
 
 pip install -r requirements.txt
